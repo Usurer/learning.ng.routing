77982d5c2c1925a9a1cf30d9f330b501f8f05abf
Usurer <ivvlg@narod.ru>
2022-04-21 00:35:02 +0300

    Secondary router outlet does not work for lazy loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const firstRoute: Route = {
+  path: 'first', component: FirstComponent
+}
+
+const routes: Routes = [
+  firstRoute,
+  {path : 'second', component: SecondComponent, outlet: "secondary" },
+  { path: 'lazy', loadChildren: () => import('./lazy/lazy/lazy.module').then(m => m.LazyModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
